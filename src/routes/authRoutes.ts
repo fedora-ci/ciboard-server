fedora-ci/ciboard-server

commit a34a468d69c15c7c1cb4b5563f2934b01a24ce21

Redirect to previous page after log in/out

src/routes/authRoutes.ts
@@ -31,6 +31,15 @@ import { samlStrategy } from '../services/cfgPassport';
 const cfg = getcfg();
 const log = debug('osci:authRoutes');
 
+function redirectBack(req, res) {
+  const redirectUrl = req.cookies?.auth_redirect;
+  if (redirectUrl) {
+    res.clearCookie('auth_redirect');
+    return res.redirect(redirectUrl);
+  }
+  return res.redirect('/');
+}
+
 export default function (app: Express) {
   app.get('/debug/auth', function (req, res) {
     /**
@@ -84,17 +93,11 @@ export default function (app: Express) {
     /**
      * This will be called after passport.authenticate('saml')
      */
-    (_req, res) => {
-      res.redirect('/');
-    }
+    redirectBack,
   );
 
   app.get('/logout', function (req, res) {
-    req.logout();
-    /*
-     * TODO: invalidate session on IP
-     */
-    res.redirect('/');
+    redirectBack(req, res);
   });
 
   app.get('/current_user', (req, res) => {

commit a87a6226e8a21bbe4436d6b5d3ff6fad2a72ef07

Fix logout
Fix the logout route to really log the user out. This was broken since
a34a468d by removing the call to the `logout()` method.

src/routes/authRoutes.ts
@@ -74,7 +74,7 @@ export default function (app: Express) {
       failureFlash: true,
       successRedirect: '/',
       failureRedirect: '/login',
-    })
+    }),
   );
 
   /**
@@ -93,11 +93,14 @@ export default function (app: Express) {
     /**
      * This will be called after passport.authenticate('saml')
      */
-    redirectBack
+    (req, res) => redirectBack(req, res),
   );
 
-  app.get('/logout', function (req, res) {
-    redirectBack(req, res);
+  app.get('/logout', function (req, res, next) {
+    req.logout({}, (err) => {
+      if (err) return next(err);
+      redirectBack(req, res);
+    });
   });
 
   app.get('/current_user', (req, res) => {